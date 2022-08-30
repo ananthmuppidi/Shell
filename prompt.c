484d0291797a8c6f7fb99acf1d926256bfbfc6b0
Ananth Muppidi <ananth.muppidi21@gmail.com>
2022-08-30 18:49:01 +0530

    Prompt

--- prompt.c
+++ prompt.c
@@ -9,12 +9,10 @@
 void prompt()
 {
     struct passwd *p = getpwuid(getuid()); // Check for NULL!
-    
+    printf("User name: %s\n", p->pw_name);
 
     char hostname[HOST_NAME_MAX + 1];
     gethostname(hostname, sizeof(hostname)); // Check the return value!
     printf("Host name: %s\n", hostname);
 }
-
-
 
